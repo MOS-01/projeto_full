3bd10e5c642625f50d5ff15db2748969dfdb4c17
Mayza <mayza.santos@outlook.com.br>
2023-06-05 00:44:23 -0300

    feat: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import "reflect-metadata";
 import express, { Application } from "express";
 import "express-async-errors";
@@ -9,6 +10,7 @@ import { contactRouters } from "./routes/contact.routes";
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/contacts", contactRouters);
